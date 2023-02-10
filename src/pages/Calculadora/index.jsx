9fb8a9975f55cf6c14bdf013ef07aecd77eb8200
rosendo <rosendc30@gmail.com>
2023-02-10 09:18:28 -0300

    tamanho da tela

--- src/pages/Calculadora/index.jsx
+++ src/pages/Calculadora/index.jsx
@@ -2,7 +2,7 @@ import "./styles.js"
 import { Container } from "./styles"
 import { Body } from "../../components/Body/index.jsx"
 
-export function Calculadora() {
+export function Calculadora() {  
   return (
     <Container>
       <Body />
